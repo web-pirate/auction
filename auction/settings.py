8bcbcad9206ed8bf9120ed1a228ef2a8a0b45c19
web-pirate <manishsinghdewas@gmail.com>
2022-11-08 21:30:55 +0530

    5

--- auction/settings.py
+++ auction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ty5n&onmx+knseagjd*9n_pq&)2x7lac%qb%&^ajjkotc5grgd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
